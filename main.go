528aeeb5280b1a29bca24a32a2f552a94816ead5
Marlon Fagundez <mjfagundez@gmail.com>
2023-03-02 10:30:57 -0500

    Refactor run func to receive a context as a param

--- main.go
+++ main.go
@@ -14,16 +14,16 @@ func handleError(err error) {
 	os.Exit(1)
 }
 
-func run() error {
-	conn, err := pgx.Connect(context.Background(), os.Getenv("DATABASE_URL"))
+func run(ctx context.Context, dbURL string) error {
+	conn, err := pgx.Connect(ctx, dbURL)
 	if err != nil {
-		handleError(fmt.Errorf("unable to connect to database: %w", err))
+		return fmt.Errorf("unable to connect to database: %w", err)
 	}
-	defer conn.Close(context.Background())
+	defer conn.Close(ctx)
 
 	var name string
 	var weight int64
-	err = conn.QueryRow(context.Background(), "select name, weight from widgets where id=$1", 42).Scan(&name, &weight)
+	err = conn.QueryRow(ctx, "select name, weight from widgets where id=$1", 42).Scan(&name, &weight)
 	if err != nil {
 		return fmt.Errorf("query row failed: %w", err)
 	}
@@ -32,7 +32,14 @@ func run() error {
 }
 
 func main() {
-	err := run()
+	ctx := context.Background()
+
+	dbURL := os.Getenv("DATABASE_URL")
+	if dbURL == "" {
+		handleError(fmt.Errorf("DATABASE_URL environment variable not set"))
+	}
+
+	err := run(ctx, dbURL)
 	if err != nil {
 		handleError(fmt.Errorf("error running query: %w", err))
 	}
